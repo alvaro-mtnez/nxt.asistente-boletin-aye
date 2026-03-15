f4fed5de2a6bb0c04c131f2e9b8f8bf6dfabb2d5
alvaro-mtnez <amconsulting.spain@gmail.com>
2026-03-15 02:17:31 +0100

    Change apple-touch-icon image in _document.js
    Updated the apple-touch-icon link to a new image.

--- pages/_document.js
+++ pages/_document.js
@@ -5,11 +5,11 @@ export default function Document() {
     <Html lang="es">
       <Head>
         <link rel="icon" href="https://www.autonomosyemprendedor.es/media/autonomosyemprendedor/images/2026/03/15/2026031501591061981.jpg" />
-        <link rel="apple-touch-icon" href="https://www.autonomosyemprendedor.es/media/autonomosyemprendedor/images/2025/09/03/2025090320300074548.png" />
+        <link rel="apple-touch-icon" href="https://www.autonomosyemprendedor.es/media/autonomosyemprendedor/images/2026/03/15/2026031502120893540.jpg" />
         <meta name="application-name" content="Maquetador AyE" />
         <meta name="apple-mobile-web-app-title" content="Maquetador AyE" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
-        <title>Maquetador de Boletines · Diario AyE</title>
+        <meta name="theme-color" content="#C22D2D" />
       </Head>
       <body>
         <Main />
